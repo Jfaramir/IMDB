d9032313df788842aa7bad88c8c93e267c1c7977
Juan Pablo Carpio Guzman <jpcarpio233@gmail.com>
2020-10-21 16:11:25 +0200

    commit con los objetos profesional creados, falta metodo en profesional.ts y mostrarlo en main.ts con el metodo.

---
+++ main.ts
@@ -0,0 +1,7 @@
+import {Proffesional} from './profesionales'
+
+var actor1 = new Proffesional("Keanu Reeves",56,"Masculino",77,180,"Negro","Café","Blanco europeo",false,"Canadiense",0,"Actor");
+var actor2 = new Proffesional("Adam Sandler",54,"Masculino",75,177,"Negro","Verdoso","Blanco europeo",false,"Estadounidense",0,"Actor");
+var actor3 = new Proffesional("Noriyuki Morita",73,"Masculino",70,160,"Negro","Marrones","Asiatico",true,"Estadounidense",0,"Actor");
+var director1 = new Proffesional("Mel Gibson",64,"Masculino",78,177,"Negro","Azul","Blanco europeo",false,"Estadounidense",2,"Director");
+var escritor1 = new Proffesional("Joanne Rowling",55,"Femenino",65,165,"Rubio","verdoso","Blanco europeo",false,"Britanica",0,"Escritora");
